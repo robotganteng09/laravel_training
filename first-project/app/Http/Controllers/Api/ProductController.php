2be602ddfbd3cdeb7bc5fd28b0f060db5a3bfaf4
robotganteng09 <arsyafaisyar@gmail.com>
2025-08-18 21:16:00 +0700

    latihan 2

--- first-project/app/Http/Controllers/Api/ProductController.php
+++ first-project/app/Http/Controllers/Api/ProductController.php
@@ -6,36 +6,37 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     // get data
-    public function index(){
+    public function index()
+    {
         $products = Product::latest()->paginate(5);
-        return new ProductResource(true,'List Data Products',$products);
-        
+        return new ProductResource(true, 'List Data Products', $products);
     }
 
     //add data
-    public function store(Request $request){
-        $validator = Validator::make($request->all(),[
-           'gambar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-           'judul' => 'required',
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'judul' => 'required',
             'deskripsi' => 'required',
             'harga' => 'required|numeric',
             'stok' => 'required|numeric'
 
-            
-        ]);
-        if($validator->fails()){
-            return response()->json($validator->errors(),422);
 
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
         //upload image
         $gambar = $request->file('gambar');
         $namaFile = $gambar->hashName();
-        $gambar->storeAs('products',$namaFile);
+        $gambar->storeAs('products', $namaFile);
 
         //create product
         $product = Product::create([
@@ -46,12 +47,56 @@ class ProductController extends Controller
             'stok' => $request->stok,
         ]);
 
-        return new ProductResource(true,'Data berhasil ditambahkan!',$product);
-
-       
+        return new ProductResource(true, 'Data berhasil ditambahkan!', $product);
     }
-     public function show($id){
-            $product = Product::find($id);
-            return new ProductResource(true,'Detail data product',$product);
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return new ProductResource(true, 'Detail data product', $product);
+    }
+
+    public function update(Request $request, $id) //update product
+    {
+        $validator = Validator::make($request->all(), [
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'judul' => 'required',
+            'deskripsi' => 'required',
+            'harga' => 'required|numeric',
+            'stok' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        };
+        $product = Product::find($id); //get product by id
+
+        if ($request->hasFile('gambar')) {
+            Storage::delete('products/' . basename($product->gambar)); //delete old image
+            $image = $request->file('gambar');
+            $image->storeAs('products', $image->hashName());
+
+            $product->update([
+                'gambar' => $image->hashName(),
+                'judul' => $request->judul,
+                'deskripsi' => $request->deskripsi,
+                'harga' => $request->harga,
+                'stok' => $request->stok,
+            ]);
+        } else {
+            $product->update([
+                'judul' => $request->judul,
+                'deskripsi' => $request->deskripsi,
+                'harga' => $request->harga,
+                'stok' => $request->stok,
+            ]);
         }
+        return new ProductResource(true, 'data product berhasil diubah', $product);
+    }
+
+    public function destroy($id){ //hapus product
+        $product = Product::find($id);
+        Storage::delete('products/' .basename($product->image));
+        $product->delete();
+        return new ProductResource(true, 'data product berhasil dihapus', null);
+    }
 }
