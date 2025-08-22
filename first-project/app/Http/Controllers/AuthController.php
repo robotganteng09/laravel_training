a2c32d517576ea2d05dcc4b30a4b8f5214a77330
robotganteng09 <arsyafaisyar@gmail.com>
2025-08-22 15:28:37 +0700

    belajar login

---
+++ first-project/app/Http/Controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string|min:4',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+            $user = User::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]);
+            return response()->json([
+                'response' => 201,
+                'status' => 'success',
+                'message' => 'Successfully registered',
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'response_code' => 422,
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        } catch (\Exception $e) {
+            Log::error('Registration Error: ' . $e->getMessage());
+
+            return response()->json([
+                'response_code' => 500,
+                'status'        => 'error',
+                'message'       => 'Registration failed',
+            ], 500);
+        }
+    }
+
+    public function Login(Request $request)
+    {
+        try {
+            $credentials = $request->validate([
+                'email' => 'reqired|email',
+                'password' => 'required|string',
+            ]);
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'response_code' => 401,
+                    'status' => 'error',
+                    'message' => 'Unauthorized',
+                ], 401);
+            }
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'response_code' => 200,
+                'status'        => 'success',
+                'message'       => 'Login successful',
+                'user_info'     => [
+                    'id'    => $user->id,
+                    'name'  => $user->name,
+                    'email' => $user->email,
+                ],
+                'token'       => $token,
+                'token_type'  => 'Bearer',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'response_code' => 422,
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        } catch (\Exception $e) {
+            Log::error('Login Error: ' . $e->getMessage());
+
+            return response()->json([
+                'response_code' => 500,
+                'status'        => 'error',
+                'message'       => 'login failed',
+            ], 500);
+        }
+    }
+}
--- first-project/routes/api.php
+++ first-project/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
 Route::apiResource('/products',ProductController::class);
 
 
